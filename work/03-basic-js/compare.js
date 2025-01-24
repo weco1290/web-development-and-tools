aceeb86be8bce53066503c6a75e60897182cb054
weco1290 <70970897+weco1290@users.noreply.github.com>
2025-01-23 20:58:06 -0800

    use frequency map to compare two words

--- work/03-basic-js/compare.js
+++ work/03-basic-js/compare.js
@@ -2,9 +2,27 @@
 /* DO NOT MODIFY EXCEPT WHERE ALLOWED */
 module.exports = compare; // DO NOT MODIFY - USED FOR TESTING
 
-function compare( word, guess ) {  // DO NOT MODIFY THIS LINE
+function compare(word, guess) {
+  // DO NOT MODIFY THIS LINE
+  word = word.toLowerCase();
+  guess = guess.toLowerCase();
 
-/* YOU MAY MODIFY THE LINES BELOW */
+  /* YOU MAY MODIFY THE LINES BELOW */
+  const createFrequencyMap = (word) => {
+    const freqMap = {};
+    for (const char of word) {
+      freqMap[char] = (freqMap[char] || 0) + 1;
+    }
+    return freqMap;
+  };
+  const freq1 = createFrequencyMap(word);
+  const freq2 = createFrequencyMap(guess);
 
-  return 0; // this line is wrong
+  let count = 0;
+  for (const char in freq1) {
+    if (freq2[char]) {
+      count += Math.min(freq1[char], freq2[char]);
+    }
+  }
+  return count;
 }
